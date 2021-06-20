05d9a5f103717b2c6b1ee2acb4c1327be44c72e0
parsakhk <parsakhk88@gmail.com>
2021-06-20 16:17:34 +0430

    yere

--- index.py
+++ index.py
@@ -21,15 +21,26 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=activity)
     print("Bot is online")
 
+client.remove_command('help')
+
+
+
+
 @client.command()
-async def ping(message, bot):
+async def ping(ctx, bot):
     if message.content.startswith('*ping'):
         embedVar = discord.Embed(title="پینگ شما!", description=(f'your ping is {bot.latency}'), color=0x00ff00)
-        embedVar.add_field(name="Field1", value="hi", inline=False)
-        embedVar.add_field(name="Field2", value="hi2", inline=False)
-        await message.channel.send(embed=embedVar)
+        await ctx.channel.send(embed=embedVar)
+    
+    
+@client.command()
+async def invite(ctx):
+    ctx.reply("لینک اینوایت خدمت شما\nhttps://discord.com/api/oauth2/authorize?client_id=856125931865505812&permissions=0&scope=bot")
 
-client.remove_command('help')
+
+
+
+
 
 
 
